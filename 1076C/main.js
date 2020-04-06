88af94a0153a5024bdbef86dbbea72e4b0141dc3
Sanjay Rai <sanjayrai@Sanjays-MacBook-Pro.local>
2020-04-06 00:05:31 -0700

    JS only has support for Floats so not sure if this is possible to get correct precisions

---
+++ 1076C/main.js
@@ -0,0 +1,71 @@
+'use strict';
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', inputStdin => {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', _ => {
+    inputString = inputString.trim().split('\n').map(string => {
+        return string.trim();
+    });
+
+    main();
+});
+
+function readline() {
+    return inputString[currentLine++];
+}
+
+function print(x) {
+    console.log(x);  // with auto '\n' (newline)
+}
+
+function printSameLine(x) {
+    process.stdout.write(x);
+}
+
+// Make a Snippet for the code above this and then write your logic in main();
+
+function main(){
+
+    var MARGIN_OF_ERROR_LIMIT = Math.pow(10, -6);
+
+
+    // Trying to get a to converge via binary search to this equation
+    // d = a^2 / (a - 1)
+    function getValue(a){
+        var numerator = Math.pow(a, 2);
+        var denominator = a - 1;
+        return numerator / denominator;
+    }
+
+    function solution(d){
+        var low = 0;
+        var high = d;
+        var mid = low + (low + high)/2;
+        var val = getValue(mid);
+
+        while(Math.abs(d - val) >= MARGIN_OF_ERROR_LIMIT){
+            if(d - val > 0) {
+                low = mid ;
+            }
+            else {
+                high = mid;
+            }
+            mid = low + (high - low)/2;
+            val = getValue(mid);
+        }
+        return val;
+    }
+
+    const n = readline();
+    solution(parseInt(n));
+}
+
+
